ce2d6629f74dea914adcb26e89425dc42de23bd6
gaoheze <gczqbx2008@vip.qq.com>
2020-07-20 09:12:10 +0800

    ①添加查询部分查询条件

--- AG.Auto.WebClient/ClientApp/src/app/app.module.ts
+++ AG.Auto.WebClient/ClientApp/src/app/app.module.ts
@@ -9,14 +9,13 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
--- AG.Auto.WebClient/ClientApp/src/app/home/home.component.ts
+++ AG.Auto.WebClient/ClientApp/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import * as XLSX from 'xlsx';
 
 
@@ -7,53 +7,22 @@ import * as XLSX from 'xlsx';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
-  public data = Array();
-  //public data2 = [];
-  //data = [
-  //  ['1', 'a', 'aa'],
-  //  ['2', 'b', 'bb'],
-  //  ['3', 'c', 'cc']
-  //]
-  //public currentPage = 1; // 当前页 （请求数据）
-  //public pageSize = 2;    // 每页记录数 （请求数据）
-  //public totalCount = 0;   // 总记录数 （响应数据）
-  //public totalPages = 0;   // 总页数 （根据 总记录数、每页记录数 计算 ）
-  //public rows = Array();
-  //// 要在分页工具条显示所有页码
-  //public pageList = new Array();
-  //prev() { };
-  //next() { };
-  //selectPage(page) {
-  //  // 分割数据
-  //  this.rows = this.datas.filter((item, index, array) => {
-  //    return index <2
-  //  });
-  //  this.totalCount = this.datas.length;
-  //  this.totalPages = Math.ceil(this.totalCount);
+  excel_data = Array();
+  query_data = Array();
+  query_where = {
+    // 铁水温度
+    q_iron_temperature_from: 1150,
+    // 铁水温度
+    q_iron_temperature_to: 1450,
+    q_iron_si:0.040,
+    q_iron_si_from: 0.01,
+    q_iron_si_to: 2.0,
+    q_steel_temperature_from: 1600,
+    q_steel_temperature_to: 1750,
+    q_steel_temperature_si:0.020,
+    q_blow_checked: false,
+  }
 
-  //  //this.pageSize = this.datas.length;
-  //  this.currentPage = page;
-  //  let begin;
-  //  let end;
-  //  begin = this.currentPage - 5;
-  //  if (begin<1) {
-  //    begin = 1;
-  //  }
-  //  end = begin + 9;
-  //  if (end>this.totalPages) {
-  //    end = this.totalPages;
-  //  }
-  //  begin = end - 9;
-  //  if (begin <1) {
-  //    begin = 1;
-  //  }
-  //  for (var i = begin; i <= end; i++) {
-  //    this.pageList.push(i);
-  //  }
-  //}
-  //isActivePage(page) {
-  //  return page === this.currentPage;
-  //}
   constructor() { }
   ngOnInit() {
     console.log("hello ngOnInit");
@@ -61,8 +30,8 @@ export class HomeComponent {
   // 导出
   daochu() {
     /* generate worksheet */
-    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
-    const ws2: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.data);
+    const ws: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.excel_data);
+    const ws2: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(this.excel_data);
 
     /* generate workbook and add the worksheet */
     const wb: XLSX.WorkBook = XLSX.utils.book_new();
@@ -93,18 +62,26 @@ export class HomeComponent {
      let xlsdata = [];
       xlsdata = (XLSX.utils.sheet_to_json(ws, { header: 0 }));
       console.log(xlsdata)
-      this.data = xlsdata.filter((item, index, array) => {        
-        return item["是否点吹"] === 0 &&
-          item["出钢温度"] != null &&
-          item["出钢温度"] != "" &&
-          (item["温度"] >= 1150 && item["温度"] <= 1450) &&
-          (item["出钢温度"] >= 1600 && item["出钢温度"] <= 1750) &&
-          item["铁水成分：P"] >= 0.040 ;
-      });
-      console.log(this.data)
+      this.excel_data = xlsdata;
+      this.select();
+      console.log(this.excel_data)
       //evt.target.value = "" // 清空
     };
     reader.readAsBinaryString(target.files[0]);
 
+  }
+
+  select() {
+    this.query_data = this.excel_data.filter((item, index, array) => {
+      return (
+        (item["温度"] >= this.query_where.q_iron_temperature_from && item["温度"] <= this.query_where.q_iron_temperature_to) &&
+        item["铁水成分：P"] >= this.query_where.q_iron_si &&
+        item["出钢温度"] !== null && item["出钢温度"] !== "" &&       
+        (item["出钢温度"] >= this.query_where.q_steel_temperature_from && item["出钢温度"] <= this.query_where.q_steel_temperature_to) &&
+        //item["出钢：P"] <= this.query_where.q_steel_temperature_si &&
+        (item["铁水成分：SI"] >= this.query_where.q_iron_si_from && item["铁水成分：SI"] <= this.query_where.q_iron_si_to) &&
+        item["是否点吹"] === (this.query_where.q_blow_checked ? 1 : 0) 
+        );
+    });
   }
 }
